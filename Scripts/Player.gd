fa7dc1fcabb7750662c9c2c5b8190bd83b89ac20
Cube Nerd <mastercuber55@gmail.com>
2026-01-08 16:23:00 +0530

    moving animation plays for both clients now

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,10 +2,26 @@ extends CharacterBody2D
 
 const SPEED = 25000.0
 
+@onready var sprite := $AnimatedSprite2D
+@export var moving := false
+
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
 
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
 		velocity = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown") * delta * SPEED
-	move_and_slide()
+	
+		if velocity == Vector2.ZERO:
+			sprite.stop()
+			moving = false
+		else:
+			moving = true
+			sprite.play("Running")	
+		
+		move_and_slide()
+	else: 
+		if moving:
+			sprite.play("Running")
+		else:
+			sprite.stop()
